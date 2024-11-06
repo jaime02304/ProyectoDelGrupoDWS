ce11558f33a810e08dca95c95e7b07edc61b0d15
DESKTOP-2U1CBE1\vspc <csi23-jpribio@colegioaltair.net>
2024-11-06 21:08:09 +0100

    Proyecto maven

---
+++ ProyectoDelGrupoDWS/src/main/java/edu/ProyectoDelGrupoDWS/ProyectoDelGrupoDwsApplication.java
@@ -0,0 +1,13 @@
+package edu.ProyectoDelGrupoDWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDelGrupoDwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDelGrupoDwsApplication.class, args);
+	}
+
+}
---
+++ ProyectoDelGrupoDWS/src/test/java/edu/ProyectoDelGrupoDWS/ProyectoDelGrupoDwsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ProyectoDelGrupoDWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDelGrupoDwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
